f73f62eb698759d46e86b98d6000cf021bfd1869
NathanielStP <nstpierre@willamette.edu>
2025-01-30 11:11:25 -0800

    Fixed errors

--- datastructures/bag.py
+++ datastructures/bag.py
@@ -1,29 +1,41 @@
 from typing import Iterable, Optional
-from ibag import IBag, T
+from datastructures.ibag import IBag, T
 
 
 class Bag(IBag[T]):
     def __init__(self, *items: Optional[Iterable[T]]) -> None:
         self.__bag: dict[T, int] = {}
-
+        
         if items != None:
             for item in items:
                 self.add(item)
 
     def add(self, item: T) -> None:
+        if item == None:
+            raise TypeError()
         if item in self.__bag:
             self.__bag[item] += 1
         else:
             self.__bag[item] = 1
 
     def remove(self, item: T) -> None:
+        thing = self.__bag.get(item)
+        if thing == None:
+            raise ValueError()
         self.__bag[item] -= 1
 
     def count(self, item: T) -> int:
-        return self.__bag.get(item)
+        output = self.__bag.get(item)
+        if output == None:
+            return 0
+        return output
 
     def __len__(self) -> int:
-        return len(self.__bag)
+        amount_list = self.__bag.values()
+        length = 0
+        for num in amount_list:
+            length += num
+        return length
 
     def distinct_items(self) -> Iterable[T]:
         return self.__bag.keys()
